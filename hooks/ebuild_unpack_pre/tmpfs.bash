b9d7eb9c43c3b4453bb1634a5023100e50522ab8
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2018-02-26 01:08:12 +0100

    hooks: add unpack_pre/tmpfs hook

---
+++ hooks/ebuild_unpack_pre/tmpfs.bash
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+hook_umount_tmpfs() {
+	local ignore_packages
+	local current_package
+	local tmpfs
+	ignore_packages=( net-www/chromium-stable dev-db/mongodb )
+	current_package="${CATEGORY}/${PN}"
+	tmpfs="/var/tmp/paludis/build/"
+
+	for package in "${ignore_packages[@]}";
+	do
+		if [[ "${current_package}" == "${package}" ]]; then
+			elog "We are installing \"${package}\" so I'm unmounting $tmpfs"
+			umount "${tmpfs}"
+			# let's clean things, just in case
+			rm "${tmpfs}/*" -rf
+			exit 0
+		fi
+	done
+
+	mount "${tmpfs}"
+	exit 0
+
+}
+
+hook_umount_tmpfs "$@"
